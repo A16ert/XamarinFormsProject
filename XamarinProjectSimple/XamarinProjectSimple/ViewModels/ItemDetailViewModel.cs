2d0f1b9b2cdba576893ecc5382fec76149e28500
Albert <fajb420@gmail.com>
2018-06-20 13:32:41 +0300

    Add project files.

---
+++ XamarinProjectSimple/XamarinProjectSimple/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinProjectSimple.Models;
+
+namespace XamarinProjectSimple.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
