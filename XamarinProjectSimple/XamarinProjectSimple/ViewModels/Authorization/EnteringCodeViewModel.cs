ccac9117d54651b271b265ee22f230f4ae82280a
Albert <fajb420@gmail.com>
2018-06-21 13:46:44 +0300

    add EnteringCodePage

---
+++ XamarinProjectSimple/XamarinProjectSimple/ViewModels/Authorization/EnteringCodeViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamarinProjectSimple.ViewModels.Authorization
+{
+    class EnteringCodeViewModel : BaseViewModel
+    {
+        string _code = string.Empty;
+        public string Code
+        {
+            get => _code;
+            set
+            {
+                _code = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public EnteringCodeViewModel()
+        {
+            SendCodeCommand = new Command(SendCode);
+        }
+        
+        public Command SendCodeCommand { get; private set; }
+
+        private void SendCode()
+        {
+            App.GoToRoot();
+        }
+    }
+}
