4f4ae1f9ccea1ff397d29bf7659bc9c05c214aa1
Albert <fajb420@gmail.com>
2018-06-27 14:01:55 +0300

    add in MainPage Shops

---
+++ XamarinProjectSimple/XamarinProjectSimple/Models/ShopModels/ShopModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace XamarinProjectSimple.Models.ShopModels
+{
+    public class ShopModel
+    {
+        public ShopModel() { }
+
+        public ShopModel(IEnumerable<ImageModel> imagesList) => ImagesList = new List<ImageModel>(imagesList);
+
+        public ShopModel(string name, string description, IEnumerable<ImageModel> imagesList)
+        {
+            Name = name;
+            Description = description;
+
+            ImagesList = new List<ImageModel>(imagesList);
+        }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public List<ImageModel> ImagesList { get; set; }
+    }
+
+    public class ImageModel
+    {
+        public ImageModel(Color color) => ColorValue = color;
+        public Color ColorValue { get; set; }
+    }
+}
---
+++ XamarinProjectSimple/XamarinProjectSimple/Views/Shop/ShopsPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinProjectSimple.Models.ShopModels;
+
+namespace XamarinProjectSimple.Views.Shop
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShopsPage : ContentPage
+    {
+        public ShopsPage()
+        {
+            InitializeComponent();
+            var imagesList = new List<ImageModel>()
+            {
+                new ImageModel(Color.Orange),
+                new ImageModel(Color.Bisque),
+                new ImageModel(Color.Blue),
+                new ImageModel(Color.Green),
+            };
+
+            MyListView.ItemsSource = new ObservableCollection<ShopModel>()
+            {
+                new ShopModel("NameTest", "DescritiropawdDescritiropawdDescritiropawdDescritiropawdDescritiropawd", imagesList),
+                new ShopModel("NameTest", "DescritiropawdDescritiropawdDescritiropawdDescritiropawdDescritiropawd", imagesList),
+                new ShopModel("NameTest", "DescritiropawdDescritiropawdDescritiropawdDescritiropawdDescritiropawd", imagesList),
+                new ShopModel("NameTest", "DescritiropawdDescritiropawdDescritiropawdDescritiropawdDescritiropawd", imagesList),
+                new ShopModel("NameTest", "DescritiropawdDescritiropawdDescritiropawdDescritiropawdDescritiropawd", imagesList),
+                new ShopModel("NameTest", "DescritiropawdDescritiropawdDescritiropawdDescritiropawdDescritiropawd", imagesList),
+                new ShopModel("NameTest", "DescritiropawdDescritiropawdDescritiropawdDescritiropawdDescritiropawd", imagesList)
+            };
+        }
+    }
+}
