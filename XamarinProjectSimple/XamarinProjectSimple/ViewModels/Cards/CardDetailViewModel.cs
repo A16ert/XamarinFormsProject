f2b9541f8dd7764cc89b6cd2ba285c1435f449fb
Albert <fajb420@gmail.com>
2018-07-05 10:52:34 +0300

    add CardDetailPage

---
+++ XamarinProjectSimple/XamarinProjectSimple/ViewModels/Cards/CardDetailViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinProjectSimple.ViewModels.Cards
+{
+    public class CardDetailViewModel : BaseViewModel
+    {
+    }
+}
