331a4d6f49fbaa1a68a52d8843d808d6b6bb3729
Albert <fajb420@gmail.com>
2018-07-18 18:35:21 +0300

    add UnderlineButton

---
+++ XamarinProjectSimple/XamarinProjectSimple.Android/Renders/ButtonRenders/ButtonUnderlineRender.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Platform.Android.AppCompat;
+using XamarinProjectSimple.Controls.Buttons;
+using XamarinProjectSimple.Droid.Renders.ButtonRenders;
+
+[assembly: ExportRenderer(typeof(ButtonUnderline), typeof(ButtonUnderlineRender))]
+namespace XamarinProjectSimple.Droid.Renders.ButtonRenders
+{
+    public class ButtonUnderlineRender : Xamarin.Forms.Platform.Android.ButtonRenderer
+    {
+        public ButtonUnderlineRender(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if(Control != null)
+            {
+                Control.PaintFlags = Android.Graphics.PaintFlags.UnderlineText;
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if(e.PropertyName == Xamarin.Forms.Button.TextProperty.PropertyName)
+            {
+                Control.Text = Element.Text;
+                Control.PaintFlags = Android.Graphics.PaintFlags.UnderlineText;
+            }
+        }
+    }
+}
---
+++ XamarinProjectSimple/XamarinProjectSimple.iOS/Renders/ButtonRenderers/ButtonUnderlineRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamarinProjectSimple.Controls.Buttons;
+
+[assembly: ExportRenderer(typeof(ButtonUnderline), typeof(XamarinProjectSimple.iOS.Renders.ButtonRenderers.ButtonUnderlineRenderer))]
+namespace XamarinProjectSimple.iOS.Renders.ButtonRenderers
+{
+    class ButtonUnderlineRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Color color = Element.TextColor;
+                var iosColor = UIColor.FromRGBA(
+                                (byte)(color.R * 255),
+                                (byte)(color.G * 255),
+                                (byte)(color.B * 255),
+                                (byte)(color.A * 255));
+
+                var underlineAttr = new UIStringAttributes { UnderlineStyle = NSUnderlineStyle.Single, ForegroundColor = iosColor };
+
+                Control.AccessibilityAttributedLabel = new NSAttributedString(Element.Text, underlineAttr);
+            }
+        }
+    }
+}
